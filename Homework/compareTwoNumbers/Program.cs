3c4a625d8c02d00b20794773c847216cfbea9a30
Pavel Kolev <67985007+MinMaxPavel@users.noreply.github.com>
2020-07-11 21:03:58 +0300

    homework commit

---
+++ Homework/compareTwoNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compareTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two valid numbers to compare");
+            Console.WriteLine("1st number: ");
+            decimal numberOne = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("2nd number: ");
+            decimal numberTwo = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("The greatest number is: {0}",Math.Max(numberOne, numberTwo));
+        }
+    }
+}
