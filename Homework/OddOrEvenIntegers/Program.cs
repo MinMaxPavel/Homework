bbcfcf444e575b396752c486588c37c9edaac8b1
Pavel Kolev <67985007+MinMaxPavel@users.noreply.github.com>
2020-07-14 22:33:42 +0300

    Commiting homework

---
+++ Homework/OddOrEvenIntegers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddOrEvenIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input an integer value:");
+            int userIntegerInput = int.Parse(Console.ReadLine());
+
+            if (userIntegerInput % 2 == 0)
+                Console.WriteLine("The integer {0} is even", userIntegerInput);
+
+            else
+                Console.WriteLine("The integer {0} is odd", userIntegerInput);
+        }
+    }
+}
---
+++ Homework/ThirdDigitIsSeven/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdDigitIsSeven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number.");
+            int yourNumber = int.Parse(Console.ReadLine());
+
+            if (yourNumber / 100 % 10 == 7)
+                Console.WriteLine("True the third digit right to left is 7");
+            else
+                Console.WriteLine("False the third digit right to left is not seven");
+        }
+    }
+}
