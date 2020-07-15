7e306a761f6175754789f1d5fff0d9e7c5c6feb5
Pavel Kolev <67985007+MinMaxPavel@users.noreply.github.com>
2020-07-15 20:37:42 +0300

    Commiting homework

---
+++ Homework/PointInACircle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointInACircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double xCenter = 0.0;
+            double yCenter = 0.0;
+            double r = 2.0;
+            Console.WriteLine("Please input x and y values");
+            Console.Write("x = ");
+            double xCenterP = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y = ");
+            double yCenterP = Convert.ToDouble(Console.ReadLine());
+            bool inside;
+            inside = ((xCenterP - xCenter) * (xCenterP - xCenter) + (yCenterP - yCenter) * (yCenterP - yCenter)) <= (r * r);
+            Console.WriteLine(inside);
+        }
+    }
+}
