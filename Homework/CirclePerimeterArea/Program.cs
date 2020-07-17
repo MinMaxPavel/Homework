62d48d0409a6ea013eaad6f5fdbdca12a4b6700f
Pavel Kolev <67985007+MinMaxPavel@users.noreply.github.com>
2020-07-17 23:33:22 +0300

    Commiting homework

---
+++ Homework/CirclePerimeterArea/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CirclePerimeterArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Radius value:");
+            double radius = double.Parse(Console.ReadLine());
+            double perimeter = 2 * Math.PI * radius;
+            double area = Math.PI * (radius * radius);
+            Console.WriteLine("Radius : {0} , Perimeter : {1:F2} , Area : {2:F2} .", radius, perimeter, area);
+        }
+    }
+}
