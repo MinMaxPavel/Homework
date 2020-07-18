a34aba3d88396756df543479df33a6ac82bd7679
Pavel Kolev <67985007+MinMaxPavel@users.noreply.github.com>
2020-07-18 23:21:37 +0300

    Commiting homework

---
+++ Homework/CheckBitAtGivenPosition/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckBitAtGivenPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input number:");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input position:");
+            int position = int.Parse(Console.ReadLine());
+            int mask = 1 << position;
+            int result = number & mask;
+            int bit = result >> position;
+            bool bitAtPosition = bit == 1;
+            Console.WriteLine("The bit at position {0} in the number {1} has value of 1: {2}", position, number, bitAtPosition);
+        }
+    }
+}
