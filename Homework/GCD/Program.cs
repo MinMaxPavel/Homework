2cdc0fb2407902b6b32bbd72bbd354e5493338c1
Pavel Kolev <67985007+MinMaxPavel@users.noreply.github.com>
2020-08-01 21:56:26 +0300

    Commiting homework

---
+++ Homework/GCD/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 2 numbers:");
+            Console.Write("A: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("B: ");
+            int b = int.Parse(Console.ReadLine());
+            int gcd = 0;
+            int q = Math.Max(Math.Abs(a), Math.Abs(b));
+            int z = Math.Min(Math.Abs(a), Math.Abs(b));
+            while (true)
+            {
+                
+                int r = q % z;
+                q = z;
+                z = r;
+                if (r == 0)
+                {
+                    gcd = q;
+                    break;
+                }
+            }
+            Console.WriteLine("The greatest common divisor for {0} and {1} is: {2}", a, b, gcd);
+        }
+    }
+}
