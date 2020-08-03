45359f15609e3fac24920eabbc83ffe62bad24d4
Pavel Kolev <67985007+MinMaxPavel@users.noreply.github.com>
2020-08-03 06:52:18 +0300

    Commiting homework

---
+++ Homework/ExchangeIfGreater/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeIfGreater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the values for a and b each on a new line:");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c;
+
+            if (a > b)
+            {
+                c = b;
+                b = a;
+                a = c;
+            }
+
+            Console.WriteLine("{0} {1}", a, b);
+        }
+    }
+}
