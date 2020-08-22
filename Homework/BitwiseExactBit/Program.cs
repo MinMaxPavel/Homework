9d696565e5b6b532649974e2b46e5fbbf37ae936
Pavel Kolev <67985007+MinMaxPavel@users.noreply.github.com>
2020-08-22 18:59:31 +0300

    Commiting homework

---
+++ Homework/BitwiseExactBit/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitwiseExactBit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int position = 3;
+            Console.WriteLine("Please input a number.");
+            uint number = uint.Parse(Console.ReadLine());
+            int mask = 1 << position;
+            int result = (int)number & mask;
+            int bit = result >> position;
+            Console.WriteLine("Bit number 3 is: {0}", bit);
+
+        }
+    }
+}
